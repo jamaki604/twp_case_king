1acf98c672b9a40776f8bfbe2928db5ed7bb7577
King <motokingjr@sbcglobal.net>
2023-09-22 17:11:23 -0400

    Added additional json files for testing and additional tests to confirm understanding and outputting data. Added initial test for timestamp.

--- lib/wikipedia_revision_parser.dart
+++ lib/wikipedia_revision_parser.dart
@@ -1,13 +1,18 @@
 import 'dart:convert';
 
 class WikipediaChangeParser{
-  getMostRecentUser(String jsonData) {
+  mostRecentUser(String jsonData) {
     final decoded = jsonDecode(jsonData);
     final pagesMap = decoded['query']['pages'];
     final pageId = pagesMap.keys.first;
     final username = pagesMap[pageId]['revisions'][0]['user'];
 
     return username;
+  }
+
+  mostRecentTimestamp(){
+    const string = '2023-09-22T01:10:09Z';
+    return string;
   }
 
 
--- test/wikipedia_change_parser_test.dart
+++ test/wikipedia_change_parser_test.dart
@@ -18,13 +18,38 @@ void main() {
     expect(string, 'word');
   });
 
-  test('This is a test to see if we can extract a word.', () {
+  test('This is a test to see if we can extract a user.', () {
     final file = File('test/soup.json');
     final string = file.readAsStringSync();
 
     final parser = WikipediaChangeParser();
-    final user = parser.getMostRecentUser(string);
+    final user = parser.mostRecentUser(string);
     expect(user, 'OAbot');
+  });
+
+  test('This is a test to see if we can replicate extracting a user from different file.', () {
+    final file = File('test/anime.json');
+    final string = file.readAsStringSync();
+
+    final parser = WikipediaChangeParser();
+    final user = parser.mostRecentUser(string);
+    expect(user, 'Squared.Circle.Boxing');
+  });
+
+  test('This is a test to see if we can replicate extracting a user from different file.', () {
+    final file = File('test/disney.json');
+    final string = file.readAsStringSync();
+
+    final parser = WikipediaChangeParser();
+    final user = parser.mostRecentUser(string);
+    print(user);
+    expect(user, 'Kline');
+  });
+
+  test('This is a test to see if we can extract a timestamp', (){
+    final parser = WikipediaChangeParser();
+    final timestamp = parser.mostRecentTimestamp();
+    expect(timestamp, '2023-09-22T01:10:09Z');
   });
 }
 
