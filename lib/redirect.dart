439f120b901a915569f448ca16257c7be977db1c
King <motokingjr@sbcglobal.net>
2023-09-30 11:40:06 -0400

    Added a Revision and Redirect class with constructors for each object.

---
+++ lib/redirect.dart
@@ -0,0 +1,6 @@
+class redirect{
+String from;
+String to;
+
+redirect(this.from, this.to);
+}
---
+++ lib/revision.dart
@@ -0,0 +1,6 @@
+class revision {
+  String username;
+  String timestamp;
+
+  revision(this.username, this.timestamp);
+}
