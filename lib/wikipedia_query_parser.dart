acb0da644e3d440d80f9ab43089d86896f7a0a8b
King <motokingjr@sbcglobal.net>
2023-09-30 14:34:35 -0400

    Removed exploratory code and unnecessary test.

--- lib/wikipedia_query_parser.dart
+++ lib/wikipedia_query_parser.dart
@@ -6,7 +6,7 @@ import 'package:twp_case_king/revision.dart';
 
 class WikipediaRevisionParser {
 
-  ParseResult mostRecentRevision(String jsonData) {
+  ParseResult parseQuery(String jsonData) {
     final decoded = jsonDecode(jsonData);
     final pagesMap = decoded['query']['pages'];
     final pageId = pagesMap.keys.first;
@@ -41,89 +41,6 @@ class WikipediaRevisionParser {
   }
 
 
-  mostRecentTimestamp(String jsonData) {
-    final decoded = jsonDecode(jsonData);
-    final pagesMap = decoded['query']['pages'];
-    final pageId = pagesMap.keys.first;
-    final timestamp = pagesMap[pageId]['revisions'][0]['timestamp'];
-
-
-    return timestamp;
-  }
-
-  String revisionUserTimestampList(String jsonData) {
-    final decoded = jsonDecode(jsonData);
-    final pagesMap = decoded['query']['pages'];
-    final pageId = pagesMap.keys.first;
-    final revisionsList = List.from(pagesMap[pageId]['revisions']);
-    final maybeThirtyRevisionList = revisionsList.take(30).toList();
-
-    final formattedList = maybeThirtyRevisionList
-        .asMap()
-        .entries
-        .map((entry) {
-      final lineNumber = entry.key + 1;
-      final revision = entry.value;
-      final user = revision['user'];
-      final timestamp = revision['timestamp'];
-      final formattedTimestamp = DateTime.parse(timestamp).toUtc().toIso8601String();
-
-      return '\n$lineNumber. User: $user \n     Timestamp: $formattedTimestamp';
-    }).toList();
-
-    final formattedString = formattedList.join('\n');
-
-    return formattedString;
-  }
-
-
-  didItRedirect(String jsonData) {
-    try {
-      final decoded = jsonDecode(jsonData);
-      final redirects = decoded['query']['redirects'];
-
-      if (redirects != null && redirects.isNotEmpty) {
-        final firstRedirect = redirects[0];
-        final from = firstRedirect['from'];
-        final to = firstRedirect['to'];
-
-        return "Redirected From: $from \nTo: $to";
-      } else {
-        return 'There was no redirect';
-      }
-    } catch (e) {
-      return "Error decoding JSON: $e";
-    }
-  }
-
-  pageDoesNotExist(String jsonData) {
-    final decoded = jsonDecode(jsonData);
-    final pagesMap = decoded['query']['pages'];
-    final pageId = pagesMap.keys.first;
-
-    if (pageId == '-1') {
-      return 'Page does not exist';
-    } else {
-      return 'page exists';
-    }
-  }
-
-  allTogetherNow(String jsonData) {
-    final decoded = jsonDecode(jsonData);
-    final pagesMap = decoded['query']['pages'];
-    final pageId = pagesMap.keys.first;
-
-    if (pageId == '-1') {
-      return 'Page does not exist';
-    }else if (pageId != '1'){
-    final result = didItRedirect (jsonData);
-    final userTimestampList = revisionUserTimestampList(jsonData);
-    return result + '\n' + userTimestampList;
-  }else {
-    final userTimestampList = revisionUserTimestampList(jsonData);
-    return userTimestampList;
-    }
-  }
 
 
 
--- test/wikipedia_query_parser_test.dart
+++ test/wikipedia_query_parser_test.dart
@@ -8,24 +8,12 @@ import 'package:twp_case_king/revision.dart';
 import 'package:twp_case_king/wikipedia_query_parser.dart';
 
 void main() {
-  test('This is a test to see if we can read the Json file', () {
-    final file = File("test/soup.json");
-    final string = file.readAsStringSync();
-    expect(string, startsWith(
-        '{"continue":{"rvcontinue":"20230607185704|1159023098","continue":"||"}'));
-  });
-
-  test('This is a test for word.', () {
-    const string = ('word');
-    expect(string, 'word');
-  });
-
   test('The first revision can be parsed from the json.', () {
     final file = File('test/soup.json');
     final string = file.readAsStringSync();
 
     final parser = WikipediaRevisionParser();
-    final ParseResult result = parser.mostRecentRevision(string);
+    final ParseResult result = parser.parseQuery(string);
     final List<Revision> revision = result.revisions;
 
     for (Revision r in revision) {
@@ -40,7 +28,7 @@ void main() {
     final string = file.readAsStringSync();
 
     final parser = WikipediaRevisionParser();
-    final ParseResult result = parser.mostRecentRevision(string);
+    final ParseResult result = parser.parseQuery(string);
     final Redirect? redirect = result.redirect;
 
     if (redirect != null) {
@@ -55,92 +43,26 @@ void main() {
     final string = file.readAsStringSync();
 
     final parser = WikipediaRevisionParser();
-    final ParseResult result = parser.mostRecentRevision(string);
+    final ParseResult result = parser.parseQuery(string);
 
     expect(result.pageExists, false);
   });
 
-  test(
-      'This is a test to see if we can replicate extracting a user from different file.', () {
-    final file = File('test/anime.json');
-    final string = file.readAsStringSync();
-
-    final parser = WikipediaRevisionParser();
-    final user = parser.mostRecentRevision(string);
-    expect(user, 'Squared.Circle.Boxing');
-  });
-
-  test(
-      'This is a test to see if we can replicate extracting a user from different file.', () {
-    final file = File('test/disney.json');
-    final string = file.readAsStringSync();
-
-    final parser = WikipediaRevisionParser();
-    final user = parser.mostRecentRevision(string);
-    print(user);
-    expect(user, 'The Herald');
-  });
-
-  test('This is a test to see if we can extract a timestamp', () {
-    final file = File('test/disney.json');
-    final string = file.readAsStringSync();
-
-    final parser = WikipediaRevisionParser();
-    final timestamp = parser.mostRecentTimestamp(string);
-    print(timestamp);
-    expect(timestamp, '2023-09-23T14:29:10Z');
-  });
-
-  test(
-      'This is a test to see if we can extract the user timestamp data and no more then 30', () {
-    final file = File('test/1993.json');
-    final string = file.readAsStringSync();
-
-    final parser = WikipediaRevisionParser();
-    final timestamp = parser.revisionUserTimestampList(string);
-    print(timestamp);
-    expect(timestamp, contains('2023-08-04T13:22:13Z'));
-  });
-
-  test('This is a test to see if we can handle redirects', () {
-    final file = File('test/disney.json');
-    final string = file.readAsStringSync();
-
-    final parser = WikipediaRevisionParser();
-    final redirect = parser.didItRedirect(string);
-    print(redirect);
-    expect(redirect, startsWith('Redirected'));
-  });
-
-  test('This is a test to see if we can handle non-existent pages', () {
-    final file = File('test/jacobking.json');
-    final string = file.readAsStringSync();
-
-    final parser = WikipediaRevisionParser();
-    final noPage = parser.pageDoesNotExist(string);
-    print(noPage);
-    expect(noPage, 'Page does not exist');
-  });
-
-  test(
-      'This is a test to see if we can checkbox everything but network error', () {
-    final file = File('test/soup.json');
-    final string = file.readAsStringSync();
-
-    final parser = WikipediaRevisionParser();
-    final workPlease = parser.allTogetherNow(string);
-    print(workPlease);
-    expect(workPlease, isNotEmpty);
-  });
 
   test('this is a test to see if url is working', () async{
     final urlThing = QueryUpdater();
     final string = await urlThing.wikipediaPageURL('disney');
 
     final parser = WikipediaRevisionParser();
-    final workPleasee = parser.allTogetherNow(string);
-    print(workPleasee);
-    expect(workPleasee, contains('Kline'));
+    final ParseResult result = parser.parseQuery(string);
+    final Redirect? redirect = result.redirect;
+
+    if (redirect != null) {
+      print('Redirect From: ${redirect.from}, Redirect To: ${redirect.to}');
+    }
+
+    expect(redirect?.to, "The Walt Disney Company");
+
   });
 }
 
